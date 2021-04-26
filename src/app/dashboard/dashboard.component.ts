e47035d0a79e9391b864e109d1211594251d3b41
SkyZyy <sergito1997.sd@gmail.com>
2021-04-26 17:49:09 +0100

    [Add] Get stats

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  url1 = "http://localhost:7101/ull-alumno/getStudentsTotal";
+  view: any[] = [700, 400];
+  single: any[] = [];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'Population';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor(private http: HttpClient) { 
+    this.http.get(this.url1).subscribe(data => {
+      this.single = JSON.parse(JSON.stringify(data));
+    })
+  }
 
   ngOnInit(): void {
+    Object.assign(this.single)
+  }
+
+  onSelect(event: any) {
+    console.log(event);
   }
 
 }
