b004828c0a6a523791845fd56a69b4f25988a645
SkyZyy <sergito1997.sd@gmail.com>
2021-05-02 19:29:45 +0100

    [Add] Barchart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Chart from 'chart.js';
+import { Observable } from 'rxjs';
 import { APIService } from '../services/api.service';
 
 @Component({
@@ -11,12 +13,12 @@ export class DashboardComponent implements OnInit {
   public chartType: string = 'bar';
   public barType: string = 'bar';
 
-  public barLabels: Array<any> = [];
-  public barData: Array<any> = [];
+  /*public barLabels: Array<any> = [];
+  public barData: Array<any> = [];*/
 
   public chartDatasets: Array<any> = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
+    //{ data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
   ];
 
   public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
@@ -48,23 +50,43 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getTotalFromApi();
-    console.log(this.barData);
-    console.log(this.barLabels);
-  }
 
-  async getTotalFromApi() {
-    await this.dataService.getTotalStudents().subscribe(data => {
-      this.single = data.DBstudentsTotalOutput;
-      var auxData = new Array();
+    // Total students bar chart
+    this.dataService.getTotalStudents().subscribe(res => {
+      let aux = res.DBstudentsTotalOutput;
+      let auxData = new Array();
+      let barLabels = new Array;
+      let barData = new Array;
 
-      for (let i in this.single){
-        this.barLabels.push(this.single[i]["COD_TITULACION"]);
-        auxData.push(this.single[i]["COUNT___"]);
+      for (let i in aux) {
+        barLabels.push(aux[i]["COD_TITULACION"]);
+        auxData.push(aux[i]["COUNT___"]);
       }
 
-      var finalData = { data: auxData, label: 'Titulaciones'};
-      this.barData.push(finalData);
+      barData.push({ data: auxData, label: 'Titulaciones', backgroundColor: 'rgba(0, 137, 132, .2)', borderColor: 'rgba(200, 99, 132, .7)', borderWidth: 2 });
+
+      new Chart('barChart', {
+        type: 'bar',
+        data: {
+          labels: barLabels,
+          datasets: barData
+        },
+        options: {
+          legend: { display: true },
+          title: {
+            display: false,
+            text: 'Total de egresados por titulacion'
+          },
+          scales: {
+            yAxes: [{
+              display: true,
+              ticks: {
+                beginAtZero: true 
+              }
+            }]
+          }
+        }
+      });
     });
   }
 }
